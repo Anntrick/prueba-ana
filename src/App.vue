c129668102c3aa2acca2472be3ab7bf3825f5e16
Anntrick <anntrick91@gmail.com>
2022-04-01 13:50:07 +0200

    estructura inicial

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <header>
+    Prueba Ana
+     <nav>
+      <router-link to="/">Inicio</router-link> |
+      <router-link to="/autor">Autor</router-link> |
+      <router-link to="/contact">Contacto</router-link>
+    </nav>
+  </header> 
+  <router-view/>
+  <footer>Ana del Pino - 2022</footer>
+</template>
+
+<style lang="scss">
+body {
+  margin: 0;
+}
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+header {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #1a8ff3;
+  color: white;
+}
+
+nav {
+  padding: 20px 30px;
+
+  a {
+    font-weight: bold;
+    color: white;
+    text-decoration: none;
+
+    &.router-link-exact-active {
+      color: white;
+    }
+  }
+}
+</style>
---
+++ src/views/ContactView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="contact">
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'ContactView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
